935574fe864175d37f54ee680f2ad9da09eb78cc
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-10-22 12:07:39 +0200

    configurate editor

--- src/components/editor.js
+++ src/components/editor.js
@@ -20,12 +20,23 @@ export default React.createClass({
     window.tinymce.init({
       selector: '#article',
       plugins: [
-        'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
-        'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-        'save table contextmenu directionality emoticons template paste textcolor'
+        'advlist autolink link image lists charmap print hr anchor pagebreak',
+        'searchreplace wordcount visualblocks visualchars code fullscreen media nonbreaking',
+        'save table contextmenu directionality emoticons template paste textcolor autosave'
       ],
-      height: 600
+      menubar: 'edit insert view format table tools',
+      toolbar: 'insertfile undo redo | styleselect | bold italic underline forecolor backcolor removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | emoticons print fullscreen',
+      height: 1000,
+      browser_spellcheck: true,
+      automatic_uploads: false,
+      statusbar: false
     })
+  },
+
+  componentWillUnmount () {
+    // this is needed for correct behaviour, see
+    // http://stackoverflow.com/questions/29169158/react-html-editor-tinymce
+    window.tinymce.remove('#article')
   },
 
   onRequestSaveCkeditor () {
