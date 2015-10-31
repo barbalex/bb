f0acd5275d3fe7317d26a8fd37d6b21d16e84318
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-10-31 22:53:09 +0100

    requery monthlyEvents when a single monthly event changes

--- src/components/monthlyEvents/monthlyEvents.js
+++ src/components/monthlyEvents/monthlyEvents.js
@@ -35,7 +35,14 @@ export default React.createClass({
   },
 
   componentDidMount () {
+    this.listenTo(app.monthlyEventStore, this.onMonthlyEventStoreChange)
     this.listenTo(app.monthlyEventsStore, this.onMonthlyEventsStoreChange)
+    app.Actions.getMonthlyEvents()
+  },
+
+  onMonthlyEventStoreChange (activeMonthlyEvent) {
+    // need to requery monthly events
+    // so new / changed arrivals and victims are shown
     app.Actions.getMonthlyEvents()
   },
 
