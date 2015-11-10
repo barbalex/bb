d9c43ffae7bdd773ebf1b36eb67f0c482d97c016
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-10 14:40:11 +0100

    get typeahead to work

--- src/components/events/tagsInput.js
+++ src/components/events/tagsInput.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { Tokenizer } from 'react-typeahead'
-import tags from './tags.js'
+import allTags from './tags.js'
 
 export default React.createClass({
   displayName: 'EventTags',
@@ -13,22 +13,55 @@ export default React.createClass({
   },
 
   onTokenAdd (tag) {
-    const { tags, onChangeTags } = this.props
+    const { onChangeTags } = this.props
+    let { tags } = this.props
     tags.push(tag)
     onChangeTags(tags)
   },
 
+  onTokenRemove (tag) {
+    const { onChangeTags } = this.props
+    let { tags } = this.props
+    tags = tags.filter((_tag) => _tag !== tag)
+    onChangeTags(tags)
+  },
+
+  onFocus () {
+    //TODO: open unfiltered list
+  },
+
+  filterOption (inputValue, option) {
+    if (inputValue === '*') return true
+    return option.includes(inputValue)
+  },
+
   render () {
+    const { tags } = this.props
     const labelStyle = {
       fontWeight: 'bold',
       marginBottom: 5
     }
+    const customClasses = {
+      input: 'form-control',
+      token: 'btn btn-default',
+      listItem: 'list-group-item',
+      results: 'list-group'
+    }
+    const allTagOptions = allTags()
+    const options = allTagOptions.filter((option) => !tags.includes(option))
     return (
       <div style={{marginBottom: 20}}>
         <div style={labelStyle}>Tags</div>
         <Tokenizer
-          options={tags()}
-          onTokenAdd={this.onTokenAdd} />
+          options={options}
+          value={tags}
+          // defaultValue='*'
+          customClasses={customClasses}
+          onTokenAdd={this.onTokenAdd}
+          onTokenRemove={this.onTokenRemove}
+          filterOption={this.filterOption}
+          placeholder='Type * to show all tags. Type text to filter them'
+          onFocus={this.onFocus} />
       </div>
     )
   }
