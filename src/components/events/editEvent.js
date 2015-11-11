6346410a72a59c11f276e5f0d3076940ada79789
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-11 11:53:11 +0100

    add close button

--- src/components/events/editEvent.js
+++ src/components/events/editEvent.js
@@ -94,8 +94,8 @@ export default React.createClass({
       marginBottom: 10
     }
     return (
-      <Modal show={true} onHide={this.close} bsSize='large'>
-        <Modal.Header>
+      <Modal show={true} onHide={this.close} bsSize='large' dialogClassName='editEvent'>
+        <Modal.Header closeButton={true}>
           <Modal.Title>Edit event</Modal.Title>
         </Modal.Header>
 
