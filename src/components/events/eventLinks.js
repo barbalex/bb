e3c4ff5e4c46f7caab2ba3e11416cb79d902a2f4
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-11 16:05:54 +0100

    remove links from state

--- src/components/events/eventLinks.js
+++ src/components/events/eventLinks.js
@@ -12,48 +12,43 @@ export default React.createClass({
     onChangeLinks: React.PropTypes.func
   },
 
-  getInitialState () {
-    const { links } = this.props
-    return { links }
-  },
-
-  onRemoveLink (indexRemoved) {
+  onRemoveLink (linkToRemove) {
     const { onChangeLinks } = this.props
-    let { links } = this.state
-    links = links.filter((link, index) => index !== indexRemoved)
-    this.setState({ links })
+    let { links } = this.props
+    console.log('eventLinks.js, onRemoveLink, linkToRemove', linkToRemove)
+    console.log('eventLinks.js, onRemoveLink, links before filtering', links)
+    links = links.filter((link, index) => link.label !== linkToRemove.label && link.url !== linkToRemove.url)
+    console.log('eventLinks.js, onRemoveLink, links after filtering', links)
     onChangeLinks(links)
   },
 
   onChangeLink (index, link) {
     const { onChangeLinks } = this.props
-    let { links } = this.state
+    let { links } = this.props
     const label = link.label
     const url = link.url
     links[index] = { label, url }
-    this.setState({ links })
     onChangeLinks(links)
   },
 
   onNewLink () {
     const { onChangeLinks } = this.props
-    let { links } = this.state
+    let { links } = this.props
     const newLink = {
       url: '',
       label: ''
     }
     links.push(newLink)
-    this.setState({ links })
     onChangeLinks(links)
   },
 
   eventLinks () {
-    const { links } = this.state
+    const { links } = this.props
     return links.map((link, index) => <EventLink key={index} index={index} links={links} link={link} focus={index === links.length - 1} onChangeLink={this.onChangeLink} onRemoveLink={this.onRemoveLink} />)
   },
 
   render () {
-    const { links } = this.state
+    const { links } = this.props
     const labelText = links.length > 0 ? 'Label' : null
     const urlText = links.length > 0 ? 'Url' : null
     const titleStyle = {
