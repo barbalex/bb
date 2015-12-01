4114e217c85dda679a51bd0afc51c0e926e73362
Alexander Gabriel <alex.barbalex@gmail.com>
2015-12-01 15:04:40 +0100

    new title

--- src/components/header.js
+++ src/components/header.js
@@ -10,17 +10,9 @@ export default React.createClass({
       <div className='masthead introHeader'>
         <div className='container'>
           <div className='row'>
-            <div className='col-xs-9'>
+            <div className='col-xs-12'>
               <div className='introText introTitle'>blue borders</div>
-              <div className='introText'>central mediterranean</div>
-            </div>
-            <div className='col-xs-3'>
-              <div className='pull-right introText introTextRight'>
-                <div>Italy</div>
-                <div>Malta</div>
-                <div>Tunisia</div>
-                <div>Libya</div>
-              </div>
+              <div className='introText'>central & eastern mediterranean</div>
             </div>
           </div>
         </div>
