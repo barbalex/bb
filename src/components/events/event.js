ea09c903bcc70f42e9fbb8d2f13846c4bc1a7206
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 00:01:21 +0200

    convert to es6 function

--- src/components/events/event.js
+++ src/components/events/event.js
@@ -4,111 +4,119 @@ import app from 'ampersand-app'
 import React from 'react'
 import { Glyphicon, Tooltip, OverlayTrigger } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'Event',
+const removeEventTooltip = () => (
+  <Tooltip id='removeThisEvent'>remove</Tooltip>
+)
 
-  propTypes: {
-    event: React.PropTypes.object,
-    onRemoveEvent: React.PropTypes.func,
-    email: React.PropTypes.string
-  },
+const editEventTooltip = () => (
+  <Tooltip id='editThisEvent'>edit</Tooltip>
+)
 
-  onRemoveEvent (doc) {
-    const { onRemoveEvent } = this.props
-    onRemoveEvent(doc)
-  },
-
-  onEditEvent () {
-    const { event } = this.props
-    app.Actions.getEvent(event._id)
-  },
-
-  removeEventTooltip () {
-    return <Tooltip id='removeThisEvent'>remove</Tooltip>
-  },
-
-  editEventTooltip () {
-    return <Tooltip id='editThisEvent'>edit</Tooltip>
-  },
-
-  removeEventGlyph () {
-    const { event } = this.props
-    const glyphStyle = {
-      fontSize: 0.9 + 'em',
-      color: 'red',
-      paddingLeft: 8,
-      cursor: 'pointer'
-    }
-    return (
-      <OverlayTrigger placement='top' overlay={this.removeEventTooltip()}>
-        <Glyphicon glyph='remove-circle' style={glyphStyle} onClick={this.onRemoveEvent.bind(this, event)} />
-      </OverlayTrigger>
-    )
-  },
-
-  editEventGlyph () {
-    const glyphStyle = {
-      fontSize: 0.9 + 'em',
-      paddingLeft: 8,
-      cursor: 'pointer'
-    }
-    return (
-      <OverlayTrigger placement='top' overlay={this.editEventTooltip()}>
-        <Glyphicon glyph='pencil' style={glyphStyle} onClick={this.onEditEvent} />
-      </OverlayTrigger>
-    )
-  },
-
-  render () {
-    const { event, email } = this.props
-    const showEditingGlyphons = !!email
-    const classNames = event.tags && event.tags.length > 0 ? event.tags.map((tag) => `event-${tag}`).join(' ') : []
-    const linkGlyphStyle = {
-      fontSize: 0.7 + 'em',
-      paddingRight: 3,
-      verticalAlign: 10 + '%'
-    }
-    const outerSpanStyle = {
-      paddingLeft: 5
-    }
-
-    const links = event.links.map((link, key) => {
-      return (
-        <span
-          key={key}
-          style={outerSpanStyle}>
-          {
-            key > 0 &&
-            ' '
-          }
-          <a
-            href={link.url}
-            target='_blank'>
-            <Glyphicon
-              glyph='new-window'
-              style={linkGlyphStyle} />
-            {link.label}
-          </a>
-        </span>
-      )
-    })
-
-    return (
-      <li
-        className={classNames}>
-        <p
-        className={classNames}>
-          {event.title} <span>{links}</span>
-          {
-            showEditingGlyphons &&
-            this.editEventGlyph()
-          }
-          {
-            showEditingGlyphons &&
-            this.removeEventGlyph()
-          }
-        </p>
-      </li>
-    )
+const removeEventGlyph = (event, onRemoveEvent) => {
+  const glyphStyle = {
+    fontSize: 0.9 + 'em',
+    color: 'red',
+    paddingLeft: 8,
+    cursor: 'pointer'
   }
-})
+  return (
+    <OverlayTrigger
+      placement='top'
+      overlay={removeEventTooltip()}
+    >
+      <Glyphicon
+        glyph='remove-circle'
+        style={glyphStyle}
+        onClick={() =>
+          onRemoveEvent(event)
+        }
+      />
+    </OverlayTrigger>
+  )
+}
+
+const editEventGlyph = (event) => {
+  const glyphStyle = {
+    fontSize: 0.9 + 'em',
+    paddingLeft: 8,
+    cursor: 'pointer'
+  }
+  return (
+    <OverlayTrigger
+      placement='top'
+      overlay={editEventTooltip()}
+    >
+      <Glyphicon
+        glyph='pencil'
+        style={glyphStyle}
+        onClick={() =>
+          app.Actions.getEvent(event._id)
+        }
+      />
+    </OverlayTrigger>
+  )
+}
+
+const Event = ({ event, email, onRemoveEvent }) => {
+  const showEditingGlyphons = !!email
+  const classNames = (
+    event.tags && event.tags.length > 0 ?
+    event.tags.map((tag) => `event-${tag}`).join(' ') :
+    []
+  )
+  const linkGlyphStyle = {
+    fontSize: `${0.7}em`,
+    paddingRight: 3,
+    verticalAlign: `${10}%`
+  }
+  const outerSpanStyle = {
+    paddingLeft: 5
+  }
+
+  const links = event.links.map((link, key) =>
+    <span
+      key={key}
+      style={outerSpanStyle}>
+      {
+        key > 0 &&
+        ' '
+      }
+      <a
+        href={link.url}
+        target='_blank'>
+        <Glyphicon
+          glyph='new-window'
+          style={linkGlyphStyle} />
+        {link.label}
+      </a>
+    </span>
+  )
+
+  return (
+    <li
+      className={classNames}>
+      <p
+      className={classNames}>
+        {event.title} <span>{links}</span>
+        {
+          showEditingGlyphons &&
+          editEventGlyph(event)
+        }
+        {
+          showEditingGlyphons &&
+          removeEventGlyph(event, onRemoveEvent)
+        }
+      </p>
+    </li>
+  )
+}
+
+Event.displayName = 'Event'
+
+Event.propTypes = {
+  event: React.PropTypes.object,
+  onRemoveEvent: React.PropTypes.func,
+  email: React.PropTypes.string
+}
+
+export default Event
