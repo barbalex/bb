f2bbf473306fe2e26aaebb52c17ccfe3a37bf954
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 00:10:39 +0200

    convert to es6 function

--- src/components/events/dateRow.js
+++ src/components/events/dateRow.js
@@ -4,55 +4,51 @@ import React from 'react'
 import moment from 'moment'
 import Event from './event.js'
 
-export default React.createClass({
-  displayName: 'DateRow',
+const mapEventComponents = (events, onRemoveEvent, email) =>
+  events.map((ev, key) =>
+    <Event
+      key={key}
+      event={ev}
+      email={email}
+      onRemoveEvent={onRemoveEvent} />
+  )
 
-  propTypes: {
-    dateRowObject: React.PropTypes.object,
-    onRemoveEvent: React.PropTypes.func,
-    email: React.PropTypes.string
-  },
+const DateRow = ({ dateRowObject: dRO, onRemoveEvent, email }) => {
+  const day = moment(dRO.date).format('D')
+  const migrationEvents = mapEventComponents(dRO.migrationEvents, onRemoveEvent, email)
+  const politicsEvents = mapEventComponents(dRO.politicsEvents, onRemoveEvent, email)
+  const dayClassName = migrationEvents.length > 0 || politicsEvents.length > 0 ? 'day dayWithEvents' : 'day'
 
-  mapEventComponents (events) {
-    const { onRemoveEvent, email } = this.props
-    return events.map((ev, key) => (
-      <Event
-        key={key}
-        event={ev}
-        email={email}
-        onRemoveEvent={onRemoveEvent} />
-      )
-    )
-  },
+  return (
+    <tr>
+      <td
+        className={dayClassName}>
+        <p>
+          {day}
+        </p>
+      </td>
+      <td
+        className='migration'>
+        <ul>
+          {migrationEvents}
+        </ul>
+      </td>
+      <td
+        className='politics'>
+        <ul>
+          {politicsEvents}
+        </ul>
+      </td>
+    </tr>
+  )
+}
 
-  render () {
-    const { dateRowObject: dRO } = this.props
-    const day = moment(dRO.date).format('D')
-    const migrationEvents = this.mapEventComponents(dRO.migrationEvents)
-    const politicsEvents = this.mapEventComponents(dRO.politicsEvents)
-    const dayClassName = migrationEvents.length > 0 || politicsEvents.length > 0 ? 'day dayWithEvents' : 'day'
+DateRow.displayName = 'DateRow'
 
-    return (
-      <tr>
-        <td
-          className={dayClassName}>
-          <p>
-            {day}
-          </p>
-        </td>
-        <td
-          className='migration'>
-          <ul>
-            {migrationEvents}
-          </ul>
-        </td>
-        <td
-          className='politics'>
-          <ul>
-            {politicsEvents}
-          </ul>
-        </td>
-      </tr>
-    )
-  }
-})
+DateRow.propTypes = {
+  dateRowObject: React.PropTypes.object,
+  onRemoveEvent: React.PropTypes.func,
+  email: React.PropTypes.string
+}
+
+export default DateRow
