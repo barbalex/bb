92539b8531fe1ee3048d107023d935fc21831b27
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 00:19:03 +0200

    convert to es6 function

--- src/components/events/monthRow.js
+++ src/components/events/monthRow.js
@@ -3,26 +3,25 @@
 import React from 'react'
 import moment from 'moment'
 
-export default React.createClass({
-  displayName: 'MonthRow',
+const MonthRow = ({ dateRowObject: dRO }) => {
+  const year = parseInt(moment(dRO.date).format('YYYY'), 0)
+  const month = moment(dRO.date).format('MMMM')
+  const text = `${month} ${year}`
+  const className = month === 'December' ? `monthRow yearRow ${year}` : 'monthRow'
 
-  propTypes: {
-    dateRowObject: React.PropTypes.object
-  },
+  return (
+    <tr className={className}>
+      <td colSpan='3'>
+        {text}
+      </td>
+    </tr>
+  )
+}
 
-  render () {
-    const { dateRowObject: dRO } = this.props
-    const year = parseInt(moment(dRO.date).format('YYYY'), 0)
-    const month = moment(dRO.date).format('MMMM')
-    const text = `${month} ${year}`
-    const className = month === 'December' ? `monthRow yearRow ${year}` : `monthRow`
+MonthRow.displayName = 'MonthRow'
 
-    return (
-      <tr className={className}>
-        <td colSpan='3'>
-          {text}
-        </td>
-      </tr>
-    )
-  }
-})
+MonthRow.propTypes = {
+  dateRowObject: React.PropTypes.object
+}
+
+export default MonthRow
