c3101d6541e1e375616e4f577f4f6b0ee68ceb20
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 16:45:03 +0200

    convert to es6 function

--- src/components/events/eventLinks.js
+++ src/components/events/eventLinks.js
@@ -5,88 +5,83 @@ import React from 'react'
 import { Row, Col, Button } from 'react-bootstrap'
 import EventLink from './eventLink.js'
 
-export default React.createClass({
-  displayName: 'EventLinks',
-
-  propTypes: {
-    activeEvent: React.PropTypes.object
-  },
-
-  onNewLink () {
-    const { activeEvent } = this.props
-    const newLink = {
-      url: '',
-      label: ''
-    }
-    activeEvent.links.push(newLink)
-    app.Actions.saveEvent(activeEvent)
-  },
-
-  eventLinks () {
-    const { activeEvent } = this.props
-    return activeEvent.links.map((link, index) => (
-        <EventLink
-          activeEvent={activeEvent}
-          link={link}
-          focus={index === activeEvent.links.length - 1}
-          key={index}
-        />
-      )
-    )
-  },
-
-  render () {
-    const { activeEvent } = this.props
-    const labelText = activeEvent.links.length > 0 ? 'Label' : null
-    const urlText = activeEvent.links.length > 0 ? 'Url' : null
-    const titleStyle = {
-      fontWeight: 'bold',
-      marginBottom: 5
-    }
-    const labelStyle = {
-      marginBottom: 0
-    }
-
-    return (
-      <div>
-        <div
-          style={titleStyle}
-        >
-          Links
-        </div>
-        <Row>
-          <Col
-            sm={3}
-            lg={2}
-          >
-            <p
-              style={labelStyle}
-            >
-              {labelText}
-            </p>
-          </Col>
-          <Col
-            sm={7}
-            lg={8}
-          >
-            <p
-              style={labelStyle}
-            >
-              {urlText}
-            </p>
-          </Col>
-          <Col
-            sm={1}
-            lg={1}
-          />
-        </Row>
-        {this.eventLinks()}
-        <Button
-          onClick={this.onNewLink}
-        >
-          new link
-        </Button>
-      </div>
-    )
+const onNewLink = (activeEvent) => {
+  const newLink = {
+    url: '',
+    label: ''
   }
-})
+  activeEvent.links.push(newLink)
+  app.Actions.saveEvent(activeEvent)
+}
+
+const eventLinks = (activeEvent) =>
+  activeEvent.links.map((link, index) =>
+    <EventLink
+      activeEvent={activeEvent}
+      link={link}
+      focus={index === activeEvent.links.length - 1}
+      key={index}
+    />
+  )
+
+const EventLinks = ({ activeEvent }) => {
+  const labelText = activeEvent.links.length > 0 ? 'Label' : null
+  const urlText = activeEvent.links.length > 0 ? 'Url' : null
+  const titleStyle = {
+    fontWeight: 'bold',
+    marginBottom: 5
+  }
+  const labelStyle = {
+    marginBottom: 0
+  }
+
+  return (
+    <div>
+      <div
+        style={titleStyle}
+      >
+        Links
+      </div>
+      <Row>
+        <Col
+          sm={3}
+          lg={2}
+        >
+          <p
+            style={labelStyle}
+          >
+            {labelText}
+          </p>
+        </Col>
+        <Col
+          sm={7}
+          lg={8}
+        >
+          <p
+            style={labelStyle}
+          >
+            {urlText}
+          </p>
+        </Col>
+        <Col
+          sm={1}
+          lg={1}
+        />
+      </Row>
+      {eventLinks(activeEvent)}
+      <Button
+        onClick={() => onNewLink(activeEvent)}
+      >
+        new link
+      </Button>
+    </div>
+  )
+}
+
+EventLinks.displayName = 'EventLinks'
+
+EventLinks.propTypes = {
+  activeEvent: React.PropTypes.object
+}
+
+export default EventLinks
