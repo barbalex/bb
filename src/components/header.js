da1a05640509d8ccbeff4179850b40933789576a
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 23:06:39 +0200

    use es6 function

--- src/components/header.js
+++ src/components/header.js
@@ -2,21 +2,18 @@
 
 import React from 'react'
 
-export default React.createClass({
-  displayName: 'Header',
-
-  render () {
-    return (
-      <div className='masthead introHeader'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-xs-12 manualHyphens'>
-              <div className='introText introTitle'>blue borders</div>
-              <div className='introText'>central & eastern mediterranean</div>
-            </div>
-          </div>
+const Header = () =>
+  <div className='masthead introHeader'>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-xs-12 manualHyphens'>
+          <div className='introText introTitle'>blue borders</div>
+          <div className='introText'>central & eastern mediterranean</div>
         </div>
       </div>
-    )
-  }
-})
+    </div>
+  </div>
+
+Header.displayName = 'Header'
+
+export default Header
