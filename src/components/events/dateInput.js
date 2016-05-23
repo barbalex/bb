e9bb44ef04b64b832f9a11bbc47226d4f7661e57
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 16:15:19 +0200

    use es6 function

--- src/components/events/dateInput.js
+++ src/components/events/dateInput.js
@@ -5,35 +5,32 @@ import DateRangePicker from 'react-bootstrap-daterangepicker'
 import moment from 'moment'
 import { FormGroup, ControlLabel, FormControl, InputGroup } from 'react-bootstrap'
 
-const EventDate = ({ date, onChangeDatePicker }) => {
-  return (
-    <FormGroup
-      controlId="date"
+const EventDate = ({ date, onChangeDatePicker }) =>
+  <FormGroup
+    controlId="date"
+  >
+    <ControlLabel>Date</ControlLabel>
+    <InputGroup
+      style={{
+        width: `${100}%`
+      }}
     >
-      <ControlLabel>Date</ControlLabel>
-      <InputGroup
-        style={{
-          width: `${100}%`
-        }}
+      <DateRangePicker
+        singleDatePicker
+        drops="up"
+        opens="left"
+        onApply={onChangeDatePicker}
       >
-        <DateRangePicker
-          singleDatePicker
-          drops="up"
-          opens="left"
-          onApply={onChangeDatePicker}
-        >
-          <FormControl
-            type="text"
-            value={moment(date, 'DD.MM.YYYY').format('DD.MM.YYYY')}
-            onChange={() => {/* react wants an onChange handler */}}
-            bsSize="small"
-            tabIndex={2}
-          />
-        </DateRangePicker>
-      </InputGroup>
-    </FormGroup>
-  )
-}
+        <FormControl
+          type="text"
+          value={moment(date, 'DD.MM.YYYY').format('DD.MM.YYYY')}
+          onChange={() => {/* react wants an onChange handler */}}
+          bsSize="small"
+          tabIndex={2}
+        />
+      </DateRangePicker>
+    </InputGroup>
+  </FormGroup>
 
 EventDate.displayName = 'EventDate'
 
