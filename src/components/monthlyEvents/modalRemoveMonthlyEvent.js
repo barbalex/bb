15feb27d5f48b3c5adc91972bde541ae8bb2ea25
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 18:27:43 +0200

    convert to es6 function

--- src/components/monthlyEvents/modalRemoveMonthlyEvent.js
+++ src/components/monthlyEvents/modalRemoveMonthlyEvent.js
@@ -5,49 +5,51 @@ import { Modal, Button } from 'react-bootstrap'
 import getYearFromEventId from '../../modules/getYearFromEventId.js'
 import getMonthFromEventId from '../../modules/getMonthFromEventId.js'
 
-export default React.createClass({
-  displayName: 'ModalRemoveMonthlyEvent',
+const ModalRemoveMonthlyEvent = ({ doc, removeMonthlyEvent }) => {
+  const year = getYearFromEventId(doc._id)
+  const month = getMonthFromEventId(doc._id)
+  const eventName = `${month} ${year}`
 
-  propTypes: {
-    doc: React.PropTypes.object,
-    removeMonthlyEvent: React.PropTypes.func
-  },
+  return (
+    <div className='static-modal'>
+      <Modal.Dialog>
+        <Modal.Header>
+          <Modal.Title>
+            Remove monthly event "{eventName}"
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Are you sure you want to remove monthly event "{eventName}"?
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            bsStyle='danger'
+            onClick={() =>
+              removeMonthlyEvent(true)
+            }
+          >
+            yes, remove!
+          </Button>
+          <Button
+            onClick={() =>
+              removeMonthlyEvent(false)
+            }
+          >
+            no!
+          </Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
+  )
+}
 
-  onHide () {
-    console.log('onHide')
-  },
+ModalRemoveMonthlyEvent.displayName = 'ModalRemoveMonthlyEvent'
 
-  onClickRemove () {
-    const { removeMonthlyEvent } = this.props
-    removeMonthlyEvent(true)
-  },
+ModalRemoveMonthlyEvent.propTypes = {
+  doc: React.PropTypes.object,
+  removeMonthlyEvent: React.PropTypes.func
+}
 
-  close () {
-    const { removeMonthlyEvent } = this.props
-    removeMonthlyEvent(false)
-  },
-
-  render () {
-    const { doc } = this.props
-    const year = getYearFromEventId(doc._id)
-    const month = getMonthFromEventId(doc._id)
-    const eventName = `${month} ${year}`
-
-    return (
-      <div className='static-modal'>
-        <Modal.Dialog onHide={this.onHide}>
-          <Modal.Header>
-            <Modal.Title>Remove monthly event "{eventName}"</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>Are you sure, you want to remove monthly event "{eventName}"?</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button bsStyle='danger' onClick={this.onClickRemove}>yes, remove!</Button>
-            <Button onClick={this.close}>no!</Button>
-          </Modal.Footer>
-        </Modal.Dialog>
-      </div>
-    )
-  }
-})
+export default ModalRemoveMonthlyEvent
