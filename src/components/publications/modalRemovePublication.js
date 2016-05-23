00d2b92450726e2489ad1383644323102c72496f
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 18:27:48 +0200

    convert to es6 function

--- src/components/publications/modalRemovePublication.js
+++ src/components/publications/modalRemovePublication.js
@@ -3,46 +3,44 @@
 import React from 'react'
 import { Modal, Button } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'ModalRemovePublication',
+const ModalRemovePublication = ({ doc, removePublication }) =>
+  <div className='static-modal'>
+    <Modal.Dialog>
+      <Modal.Header>
+        <Modal.Title>
+          Remove publication "{doc.title}"
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          Are you sure you want to remove publication "{doc.title}"?
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          bsStyle='danger'
+          onClick={() =>
+            removePublication(true)
+          }
+        >
+          yes, remove!
+        </Button>
+        <Button
+          onClick={() =>
+            removePublication(false)
+          }
+        >
+          no!
+        </Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  </div>
 
-  propTypes: {
-    doc: React.PropTypes.object,
-    removePublication: React.PropTypes.func
-  },
+ModalRemovePublication.displayName = 'ModalRemovePublication'
 
-  onHide () {
-    console.log('onHide')
-  },
+ModalRemovePublication.propTypes = {
+  doc: React.PropTypes.object,
+  removePublication: React.PropTypes.func
+}
 
-  onClickRemove () {
-    const { removePublication } = this.props
-    removePublication(true)
-  },
-
-  close () {
-    const { removePublication } = this.props
-    removePublication(false)
-  },
-
-  render () {
-    const { doc } = this.props
-
-    return (
-      <div className='static-modal'>
-        <Modal.Dialog onHide={this.onHide}>
-          <Modal.Header>
-            <Modal.Title>Remove publication "{doc.title}"</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>Are you sure, you want to remove publication "{doc.title}"?</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button bsStyle='danger' onClick={this.onClickRemove}>yes, remove!</Button>
-            <Button onClick={this.close}>no!</Button>
-          </Modal.Footer>
-        </Modal.Dialog>
-      </div>
-    )
-  }
-})
+export default ModalRemovePublication
