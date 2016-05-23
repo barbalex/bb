9309d3def4eca6aca686451cad4db8413dd7375e
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 17:26:35 +0200

    convert to es6 function

--- src/components/events/monthlyStatisticsRow.js
+++ src/components/events/monthlyStatisticsRow.js
@@ -4,55 +4,52 @@ import React from 'react'
 import moment from 'moment'
 import Event from './event.js'
 
-export default React.createClass({
-  displayName: 'DateRow',
+const mapEventComponents = (events, onRemoveEvent, email) =>
+  events.map((ev, key) =>
+    <Event
+      key={key}
+      event={ev}
+      email={email}
+      onRemoveEvent={onRemoveEvent}
+    />
+  )
 
-  propTypes: {
-    dateRowObject: React.PropTypes.object,
-    onRemoveEvent: React.PropTypes.func,
-    email: React.PropTypes.string
-  },
+const MonthlyStatisticsRow = ({ dateRowObject: dRO, onRemoveEvent, email }) => {
+  const migrationEvents = mapEventComponents(dRO.migrationEvents, onRemoveEvent, email)
+  const politicsEvents = mapEventComponents(dRO.politicsEvents, onRemoveEvent, email)
 
-  mapEventComponents (events) {
-    const { onRemoveEvent, email } = this.props
-    return events.map((ev, key) => (
-      <Event
-        key={key}
-        event={ev}
-        email={email}
-        onRemoveEvent={onRemoveEvent} />
-      )
+  if (migrationEvents.length > 0 || politicsEvents.length > 0) {
+    return (
+      <tr className='monthlyStatisticsRow'>
+        <td
+          className='day'>
+          <p>
+          </p>
+        </td>
+        <td
+          className='migration'>
+          <ul>
+            {migrationEvents}
+          </ul>
+        </td>
+        <td
+          className='politics'>
+          <ul>
+            {politicsEvents}
+          </ul>
+        </td>
+      </tr>
     )
-  },
-
-  render () {
-    const { dateRowObject: dRO } = this.props
-    const migrationEvents = this.mapEventComponents(dRO.migrationEvents)
-    const politicsEvents = this.mapEventComponents(dRO.politicsEvents)
-
-    if (migrationEvents.length > 0 || politicsEvents.length > 0) {
-      return (
-        <tr className='monthlyStatisticsRow'>
-          <td
-            className='day'>
-            <p>
-            </p>
-          </td>
-          <td
-            className='migration'>
-            <ul>
-              {migrationEvents}
-            </ul>
-          </td>
-          <td
-            className='politics'>
-            <ul>
-              {politicsEvents}
-            </ul>
-          </td>
-        </tr>
-      )
-    }
-    return null
   }
-})
+  return null
+}
+
+MonthlyStatisticsRow.displayName = 'MonthlyStatisticsRow'
+
+MonthlyStatisticsRow.propTypes = {
+  dateRowObject: React.PropTypes.object,
+  onRemoveEvent: React.PropTypes.func,
+  email: React.PropTypes.string
+}
+
+export default MonthlyStatisticsRow
