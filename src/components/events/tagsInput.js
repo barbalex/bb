731c658922e8bcd5d42bdedc1c8b3d356ee876ec
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 16:15:45 +0200

    use es6 function

--- src/components/events/tagsInput.js
+++ src/components/events/tagsInput.js
@@ -5,79 +5,77 @@ import React from 'react'
 import { Glyphicon } from 'react-bootstrap'
 import allTags from './tags.js'
 
-export default React.createClass({
-  displayName: 'EventTags',
+const tagIcon = (option) => {
+  const top = option.top ? option.top : 0
+  const glyphStyle = {
+    top: top,
+    fontSize: 1.5 + 'em'
+  }
+  return (
+    <Glyphicon
+      glyph={option.iconText}
+      style={glyphStyle}
+    />
+  )
+}
 
-  propTypes: {
-    activeEvent: React.PropTypes.object
-  },
-
-  onChangeTag (tag, event) {
-    let { activeEvent } = this.props
-    const checked = event.target.checked
-    if (checked) {
-      activeEvent.tags.push(tag)
-      app.Actions.saveEvent(activeEvent)
-    } else {
-      activeEvent.tags = activeEvent.tags.filter((_tag) => _tag !== tag)
-      app.Actions.saveEvent(activeEvent)
-    }
-  },
-
-  tagIcon (option) {
-    const top = option.top ? option.top : 0
-    const glyphStyle = {
-      top: top,
-      fontSize: 1.5 + 'em'
-    }
-    return (
-      <Glyphicon glyph={option.iconText} style={glyphStyle} />
-    )
-  },
-
-  tags () {
-    const { activeEvent } = this.props
-    const options = allTags()
-    return options.map((option, index) => {
-      const checked = activeEvent.tags.includes(option.tag)
-      return (
-        <div
-          key={index}
-          className='form-group event-tag'>
-          <label>
-            <input
-              type='checkbox'
-              checked={checked}
-              onChange={this.onChangeTag.bind(this, option.tag)} />
-              {
-                option.iconText &&
-                this.tagIcon(option)
-              }
-              {option.tag}
-          </label>
-        </div>
-      )
-    })
-  },
-
-  render () {
-    const labelStyle = {
-      fontWeight: 'bold',
-      marginBottom: 2
-    }
-
+const tags = (activeEvent) => {
+  const options = allTags()
+  return options.map((option, index) => {
+    const checked = activeEvent.tags.includes(option.tag)
     return (
       <div
-        style={{marginBottom: 20}}>
-        <div
-          style={labelStyle}>
-          Tags
-        </div>
-        <div
-          className='event-tags'>
-          {this.tags()}
-        </div>
+        key={index}
+        className='form-group event-tag'>
+        <label>
+          <input
+            type='checkbox'
+            checked={checked}
+            onChange={(event) => onChangeTag(option.tag, event, activeEvent)} />
+            {
+              option.iconText &&
+              tagIcon(option)
+            }
+            &nbsp;{option.tag}
+        </label>
       </div>
     )
+  })
+}
+
+const onChangeTag = (tag, event, activeEvent) => {
+  const checked = event.target.checked
+  if (checked) {
+    activeEvent.tags.push(tag)
+    app.Actions.saveEvent(activeEvent)
+  } else {
+    activeEvent.tags = activeEvent.tags.filter((_tag) => _tag !== tag)
+    app.Actions.saveEvent(activeEvent)
   }
-})
+}
+
+const labelStyle = {
+  fontWeight: 'bold',
+  marginBottom: 2
+}
+
+const EventTags = ({ activeEvent }) =>
+  <div
+    style={{marginBottom: 20}}>
+    <div
+      style={labelStyle}>
+      Tags
+    </div>
+    <div
+      className='event-tags'>
+      {tags(activeEvent)}
+    </div>
+  </div>
+
+EventTags.displayName = 'EventTags'
+
+EventTags.propTypes = {
+  activeEvent: React.PropTypes.object
+}
+
+export default EventTags
