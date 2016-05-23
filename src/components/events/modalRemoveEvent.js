76e45d98344af222c26467daa35df692d06e67b4
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 17:03:49 +0200

    use es6 function

--- src/components/events/modalRemoveEvent.js
+++ src/components/events/modalRemoveEvent.js
@@ -3,58 +3,39 @@
 import React from 'react'
 import { Modal, Button } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'ModalRemoveEvent',
+const ModalRemoveEvent = ({ doc, removeEvent }) =>
+  <div className='static-modal'>
+    <Modal.Dialog
+      onHide={this.onHide}>
+      <Modal.Header>
+        <Modal.Title>
+          Remove event "{doc.title}"
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          Are you sure, you want to remove event "{doc.title}"?
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          bsStyle='danger'
+          onClick={() => removeEvent(true)}>
+          yes, remove!
+        </Button>
+        <Button
+          onClick={() => removeEvent(false)}>
+          no!
+        </Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  </div>
 
-  propTypes: {
-    doc: React.PropTypes.object,
-    removeEvent: React.PropTypes.func
-  },
+ModalRemoveEvent.displayName = 'ModalRemoveEvent'
 
-  onHide () {
-    console.log('onHide')
-  },
+ModalRemoveEvent.propTypes = {
+  doc: React.PropTypes.object,
+  removeEvent: React.PropTypes.func
+}
 
-  onClickRemove () {
-    const { removeEvent } = this.props
-    removeEvent(true)
-  },
-
-  close () {
-    const { removeEvent } = this.props
-    removeEvent(false)
-  },
-
-  render () {
-    const { doc } = this.props
-
-    return (
-      <div className='static-modal'>
-        <Modal.Dialog
-          onHide={this.onHide}>
-          <Modal.Header>
-            <Modal.Title>
-              Remove event "{doc.title}"
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>
-              Are you sure, you want to remove event "{doc.title}"?
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button
-              bsStyle='danger'
-              onClick={this.onClickRemove}>
-              yes, remove!
-            </Button>
-            <Button
-              onClick={this.close}>
-              no!
-            </Button>
-          </Modal.Footer>
-        </Modal.Dialog>
-      </div>
-    )
-  }
-})
+export default ModalRemoveEvent
