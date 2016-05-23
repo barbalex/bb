50bdd0937942db1e96c576db7d1536925f15778e
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-23 18:27:36 +0200

    convert to es6 function

--- src/components/commentaries/modalRemoveCommentary.js
+++ src/components/commentaries/modalRemoveCommentary.js
@@ -3,57 +3,44 @@
 import React from 'react'
 import { Modal, Button } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'ModalRemoveCommentary',
+const ModalRemoveCommentary = ({ doc, removeCommentary }) =>
+  <div className='static-modal'>
+    <Modal.Dialog>
+      <Modal.Header>
+        <Modal.Title>
+          Remove commentary "{doc.title}"
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          Are you sure you want to remove commentary "{doc.title}"?
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          bsStyle='danger'
+          onClick={() =>
+            removeCommentary(true)
+          }
+        >
+          yes, remove!
+        </Button>
+        <Button
+          onClick={() =>
+            removeCommentary(false)
+          }
+        >
+          no!
+        </Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  </div>
 
-  propTypes: {
-    doc: React.PropTypes.object,
-    removeCommentary: React.PropTypes.func
-  },
+ModalRemoveCommentary.displayName = 'ModalRemoveCommentary'
 
-  onHide () {
-    console.log('onHide')
-  },
+ModalRemoveCommentary.propTypes = {
+  doc: React.PropTypes.object,
+  removeCommentary: React.PropTypes.func
+}
 
-  onClickRemove () {
-    const { removeCommentary } = this.props
-    removeCommentary(true)
-  },
-
-  close () {
-    const { removeCommentary } = this.props
-    removeCommentary(false)
-  },
-
-  render () {
-    const { doc } = this.props
-
-    return (
-      <div className='static-modal'>
-        <Modal.Dialog onHide={this.onHide}>
-          <Modal.Header>
-            <Modal.Title>
-              Remove commentary "{doc.title}"
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>
-              Are you sure, you want to remove commentary "{doc.title}"?
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button
-              bsStyle='danger'
-              onClick={this.onClickRemove}>
-              yes, remove!
-            </Button>
-            <Button
-              onClick={this.close}>
-              no!
-            </Button>
-          </Modal.Footer>
-        </Modal.Dialog>
-      </div>
-    )
-  }
-})
+export default ModalRemoveCommentary
