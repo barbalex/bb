599eace1276c3eed29fedbaa6fc53b3987949195
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-24 11:48:59 +0200

    use es6 function

--- src/components/attachImages.js
+++ src/components/attachImages.js
@@ -4,52 +4,53 @@ import app from 'ampersand-app'
 import React from 'react'
 import Dropzone from 'react-dropzone'
 
-export default React.createClass({
-  displayName: 'AttachImages',
+const onDrop = (files, doc) => {
+  let attachments = {}
+  files.forEach((file) => {
+    /**
+     * create an attachments object of this form:
+     * {
+     *   "att.txt": {
+     *     "content_type": "image/png",
+     *     "data": new Blob(
+     *       ["And she's hooked to the silver screen"],
+     *       {type: 'text/plain'})
+     *   },
+     *   "att2.txt": {
+     *     "content_type": "text/plain",
+     *     "data": new Blob(
+     *       ["But the film is a saddening bore"],
+     *       {type: 'text/plain'})
+     *   }
+     * }
+     * note: react-dropzone built the blob itself! It is file
+     */
+    const name = file.name
+    const type = file.type
+    attachments[name] = {
+      content_type: type,
+      data: file
+    }
+  })
+  app.Actions.addPageAttachments(doc, attachments)
+}
 
-  propTypes: {
-    doc: React.PropTypes.object
-  },
-
-  onDrop (files) {
-    const { doc } = this.props
-    let attachments = {}
-    files.forEach((file) => {
-      /**
-       * create an attachments object of this form:
-       * {
-       *   "att.txt": {
-       *     "content_type": "image/png",
-       *     "data": new Blob(
-       *       ["And she's hooked to the silver screen"],
-       *       {type: 'text/plain'})
-       *   },
-       *   "att2.txt": {
-       *     "content_type": "text/plain",
-       *     "data": new Blob(
-       *       ["But the film is a saddening bore"],
-       *       {type: 'text/plain'})
-       *   }
-       * }
-       * note: react-dropzone built the blob itself! It is file
-       */
-      const name = file.name
-      const type = file.type
-      attachments[name] = {
-        content_type: type,
-        data: file
-      }
-    })
-    app.Actions.addPageAttachments(doc, attachments)
-  },
-
-  render () {
-    return (
-      <div className='dropzone'>
-        <Dropzone onDrop={this.onDrop}>
-          <div>Drop some files here.<br/>Or click to select files to upload.</div>
-        </Dropzone>
+const AttachImages = ({ doc }) =>
+  <div className='dropzone'>
+    <Dropzone
+      onDrop={(event) => onDrop(event, doc)}
+    >
+      <div>
+        Drop some files here.<br/>
+        Or click to select files to upload.
       </div>
-    )
-  }
-})
+    </Dropzone>
+  </div>
+
+AttachImages.displayName = 'AttachImages'
+
+AttachImages.propTypes = {
+  doc: React.PropTypes.object
+}
+
+export default AttachImages
