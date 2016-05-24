390801e6d0a537a6888680883eff82d26ec5fa77
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-24 11:03:57 +0200

    use es6 function

--- src/components/publications/publication.js
+++ src/components/publications/publication.js
@@ -4,31 +4,34 @@ import React from 'react'
 import { Base64 } from 'js-base64'
 import Editor from '../editor.js'
 
-export default React.createClass({
-  displayName: 'Publication',
-
-  propTypes: {
-    activePublication: React.PropTypes.object,
-    editing: React.PropTypes.bool,
-    onSavePublicationArticle: React.PropTypes.func
-  },
-
-  render () {
-    const { activePublication, editing, onSavePublicationArticle } = this.props
-    const articleEncoded = activePublication.article
-    const articleDecoded = Base64.decode(articleEncoded)
-    if (editing) {
-      return (
-        <div className='publication'>
-          <Editor doc={activePublication} articleDecoded={articleDecoded} onSavePublicationArticle={onSavePublicationArticle} />
-        </div>
-      )
-    }
-    const createMarkup = () => ({__html: articleDecoded})
+const Publication = ({ activePublication, editing, onSavePublicationArticle }) => {
+  const articleEncoded = activePublication.article
+  const articleDecoded = Base64.decode(articleEncoded)
+  if (editing) {
     return (
       <div className='publication'>
-        <div dangerouslySetInnerHTML={createMarkup()} />
+        <Editor
+          doc={activePublication}
+          articleDecoded={articleDecoded}
+          onSavePublicationArticle={onSavePublicationArticle}
+        />
       </div>
     )
   }
-})
+  const createMarkup = () => ({__html: articleDecoded})
+  return (
+    <div className='publication'>
+      <div dangerouslySetInnerHTML={createMarkup()} />
+    </div>
+  )
+}
+
+Publication.displayName = 'Publication'
+
+Publication.propTypes = {
+  activePublication: React.PropTypes.object,
+  editing: React.PropTypes.bool,
+  onSavePublicationArticle: React.PropTypes.func
+}
+
+export default Publication
