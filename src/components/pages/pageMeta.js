23eb7ece1ddacd7fdd9da09b47c1f42d7f709edb
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-24 11:03:30 +0200

    use es6 function

--- src/components/pages/pageMeta.js
+++ src/components/pages/pageMeta.js
@@ -4,41 +4,37 @@ import React from 'react'
 import { Modal, Button } from 'react-bootstrap'
 import AttachedImages from '../attachedImages.js'
 
-export default React.createClass({
-  displayName: 'PageMeta',
+const PageMeta = ({ doc, onCloseMeta }) =>
+  <Modal
+    show
+      bsSize='large'
+  >
+    <Modal.Header>
+      <Modal.Title>
+        Images for "{doc.title ? doc.title : doc.category}"
+      </Modal.Title>
+    </Modal.Header>
 
-  propTypes: {
-    doc: React.PropTypes.object,
-    onCloseMeta: React.PropTypes.func
-  },
+    <Modal.Body>
+      <AttachedImages doc={doc} />
+    </Modal.Body>
 
-  close () {
-    const { onCloseMeta } = this.props
-    onCloseMeta()
-  },
+    <Modal.Footer>
+      <Button
+        bsStyle='primary'
+        onClick={() => onCloseMeta()}
+      >
+        close
+      </Button>
+    </Modal.Footer>
 
-  onHide () {
-    // seems that this method is needed ???
-  },
+  </Modal>
 
-  render () {
-    const { doc } = this.props
-    let title = doc.title ? doc.title : doc.category
-    return (
-      <Modal show onHide={this.close} bsSize='large'>
-        <Modal.Header>
-          <Modal.Title>Images for "{title}"</Modal.Title>
-        </Modal.Header>
+PageMeta.displayName = 'PageMeta'
 
-        <Modal.Body>
-          <AttachedImages doc={doc} />
-        </Modal.Body>
+PageMeta.propTypes = {
+  doc: React.PropTypes.object,
+  onCloseMeta: React.PropTypes.func
+}
 
-        <Modal.Footer>
-          <Button bsStyle='primary' onClick={this.close}>close</Button>
-        </Modal.Footer>
-
-      </Modal>
-    )
-  }
-})
+export default PageMeta
