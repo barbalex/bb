5cbbc9c0239d79bcbecc7962a0b58b5e8bde2d1e
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-24 11:49:58 +0200

    optimize code

--- src/modules/getPublications.js
+++ src/modules/getPublications.js
@@ -4,19 +4,21 @@ import app from 'ampersand-app'
 import { map } from 'lodash'
 import sortPublications from './sortPublications.js'
 
-export default () => {
-  return new Promise((resolve, reject) => {
-    const options = {
-      include_docs: true,
-      startkey: 'publications_',
-      endkey: 'publications_\uffff'
-    }
+const options = {
+  include_docs: true,
+  startkey: 'publications_',
+  endkey: 'publications_\uffff'
+}
+
+export default () =>
+  new Promise((resolve, reject) =>
     app.db.allDocs(options)
       .then((result) => {
         let publications = map(result.rows, 'doc')
         publications = sortPublications(publications)
         resolve(publications)
       })
-      .catch((error) => reject('Error fetching publications:', error))
-  })
-}
+      .catch((error) =>
+        reject('Error fetching publications:', error)
+      )
+  )
--- src/modules/getYearOfFirstEvent.js
+++ src/modules/getYearOfFirstEvent.js
@@ -3,21 +3,23 @@
 import app from 'ampersand-app'
 import getYearFromEventId from './getYearFromEventId.js'
 
-export default () => {
-  return new Promise((resolve, reject) => {
-    const options = {
-      include_docs: false,
-      startkey: `events_0000`,
-      endkey: `events_9999_\uffff`,
-      descending: false,
-      limit: 1
-    }
+const options = {
+  include_docs: false,
+  startkey: `events_0000`,
+  endkey: `events_9999_\uffff`,
+  descending: false,
+  limit: 1
+}
+
+export default () =>
+  new Promise((resolve, reject) =>
     app.db.allDocs(options)
       .then((result) => {
         const id = result.rows[0].id
         const year = getYearFromEventId(id)
         resolve(year)
       })
-      .catch((error) => reject('Error fetching events:', error))
-  })
-}
+      .catch((error) =>
+        reject('Error fetching events:', error)
+      )
+  )
