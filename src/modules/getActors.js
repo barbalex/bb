f1fac46582b56653744896addb5459ec9f29d164
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-24 11:49:38 +0200

    optimize code

--- src/modules/getActors.js
+++ src/modules/getActors.js
@@ -4,19 +4,21 @@ import app from 'ampersand-app'
 import { map } from 'lodash'
 import sortActors from './sortActors.js'
 
-export default () => {
-  return new Promise((resolve, reject) => {
-    const options = {
-      include_docs: true,
-      startkey: 'actors_',
-      endkey: 'actors_\uffff'
-    }
+const options = {
+  include_docs: true,
+  startkey: 'actors_',
+  endkey: 'actors_\uffff'
+}
+
+export default () =>
+  new Promise((resolve, reject) =>
     app.db.allDocs(options)
       .then((result) => {
         let actors = map(result.rows, 'doc')
         actors = sortActors(actors)
         resolve(actors)
       })
-      .catch((error) => reject('Error fetching actors:', error))
-  })
-}
+      .catch((error) =>
+        reject('Error fetching actors:', error)
+      )
+  )
