7766aeadeede954417bdb52435f11e8647fe43e5
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-25 14:34:31 +0200

    use es6 function

--- src/components/events/dateRows.js
+++ src/components/events/dateRows.js
@@ -8,98 +8,97 @@ import MonthRow from './monthRow.js'
 import MonthlyStatisticsRow from './monthlyStatisticsRow.js'
 import getDaterowObjectsSinceOldestEvent from '../../modules/getDaterowObjectsSinceOldestEvent.js'
 
-export default React.createClass({
-  displayName: 'DateRows',
-
-  propTypes: {
-    events: React.PropTypes.array,
-    email: React.PropTypes.string,
-    activeEventYears: React.PropTypes.array,
-    onRemoveEvent: React.PropTypes.func
-  },
-
-  render () {
-    const { events, email, activeEventYears, onRemoveEvent } = this.props
-    const dateRowObjects = getDaterowObjectsSinceOldestEvent(events, activeEventYears)
-    let dateRows = []
-    if (dateRowObjects.length > 0) {
-      dateRowObjects.forEach((dRO, index) => {
-        const day = moment(dRO.date).format('D')
-        const endOfMonth = moment(dRO.date).endOf('month').format('DD')
-        const dROForDateRow = {
-          date: dRO.date,
-          migrationEvents: dRO.migrationEvents.filter((event) =>
-            !event.tags || !event.tags.includes('monthlyStatistics')
-          ),
-          politicsEvents: dRO.politicsEvents.filter((event) =>
-            !event.tags || !event.tags.includes('monthlyStatistics')
-          )
-        }
-        const dROForMonthlyStatsRow = {
-          date: dRO.date,
-          migrationEvents: dRO.migrationEvents.filter((event) =>
-            event.tags && event.tags.includes('monthlyStatistics')
-          ),
-          politicsEvents: dRO.politicsEvents.filter((event) =>
-            event.tags && event.tags.includes('monthlyStatistics')
-          )
-        }
-        const dROForMonthlyStatsHasEvents = (
-          dROForMonthlyStatsRow.migrationEvents.length > 0 ||
-          dROForMonthlyStatsRow.politicsEvents.length > 0
+const DateRows = ({ events, email, activeEventYears, onRemoveEvent }) => {
+  const dateRowObjects = getDaterowObjectsSinceOldestEvent(events, activeEventYears)
+  let dateRows = []
+  if (dateRowObjects.length > 0) {
+    dateRowObjects.forEach((dRO, index) => {
+      const day = moment(dRO.date).format('D')
+      const endOfMonth = moment(dRO.date).endOf('month').format('DD')
+      const dROForDateRow = {
+        date: dRO.date,
+        migrationEvents: dRO.migrationEvents.filter((event) =>
+          !event.tags || !event.tags.includes('monthlyStatistics')
+        ),
+        politicsEvents: dRO.politicsEvents.filter((event) =>
+          !event.tags || !event.tags.includes('monthlyStatistics')
         )
-        const needsMonthRow = (
-          day === endOfMonth ||
-          index === 0
+      }
+      const dROForMonthlyStatsRow = {
+        date: dRO.date,
+        migrationEvents: dRO.migrationEvents.filter((event) =>
+          event.tags && event.tags.includes('monthlyStatistics')
+        ),
+        politicsEvents: dRO.politicsEvents.filter((event) =>
+          event.tags && event.tags.includes('monthlyStatistics')
         )
-        const needsMonthlyStatisticsRow = (
-          day === endOfMonth &&
-          dROForMonthlyStatsHasEvents
-        )
-        if (needsMonthRow) {
-          dateRows.push(
-            <MonthRow
-              key={`${index}monthRow`}
-              dateRowObject={dRO}
-            />
-          )
-        }
-        if (needsMonthlyStatisticsRow) {
-          dateRows.push(
-            <MonthlyStatisticsRow
-              key={`${index}monthlyStatisticsRow`}
-              dateRowObject={dROForMonthlyStatsRow}
-              email={email}
-              onRemoveEvent={onRemoveEvent}
-            />
-          )
-        }
+      }
+      const dROForMonthlyStatsHasEvents = (
+        dROForMonthlyStatsRow.migrationEvents.length > 0 ||
+        dROForMonthlyStatsRow.politicsEvents.length > 0
+      )
+      const needsMonthRow = (
+        day === endOfMonth ||
+        index === 0
+      )
+      const needsMonthlyStatisticsRow = (
+        day === endOfMonth &&
+        dROForMonthlyStatsHasEvents
+      )
+      if (needsMonthRow) {
         dateRows.push(
-          <DateRow
-            key={index}
-            dateRowObject={dROForDateRow}
+          <MonthRow
+            key={`${index}monthRow`}
+            dateRowObject={dRO}
+          />
+        )
+      }
+      if (needsMonthlyStatisticsRow) {
+        dateRows.push(
+          <MonthlyStatisticsRow
+            key={`${index}monthlyStatisticsRow`}
+            dateRowObject={dROForMonthlyStatsRow}
             email={email}
             onRemoveEvent={onRemoveEvent}
           />
         )
-      })
-      const renderDateRow = (index, key) => dateRows[index]
-      return (
-        <div>
-          <ReactList
-            itemRenderer={renderDateRow}
-            length={dateRows.length}
-            type='variable'
-          />
-        </div>
+      }
+      dateRows.push(
+        <DateRow
+          key={index}
+          dateRowObject={dROForDateRow}
+          email={email}
+          onRemoveEvent={onRemoveEvent}
+        />
       )
-    }
+    })
+    const renderDateRow = (index, key) => dateRows[index]
     return (
-      <div className="eventsTable-body-row">
-        <div className="eventsTable-body-cell">
-          <p>Loading events...</p>
-        </div>
+      <div>
+        <ReactList
+          itemRenderer={renderDateRow}
+          length={dateRows.length}
+          type='variable'
+        />
       </div>
     )
   }
-})
+  return (
+    <div className="eventsTable-body-row">
+      <div className="eventsTable-body-cell">
+        <p>Loading events...</p>
+      </div>
+    </div>
+  )
+}
+
+DateRows.displayName = 'DateRows'
+
+DateRows.propTypes = {
+  events: React.PropTypes.array,
+  email: React.PropTypes.string,
+  activeEventYears: React.PropTypes.array,
+  onRemoveEvent: React.PropTypes.func
+}
+
+export default DateRows
