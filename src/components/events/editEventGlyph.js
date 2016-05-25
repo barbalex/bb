6f0e16d4452bc4bb0c0b1d6a9afa0ffbb0f97682
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-25 15:26:57 +0200

    optimize code

---
+++ src/components/events/editEventGlyph.js
@@ -0,0 +1,37 @@
+'use strict'
+
+import app from 'ampersand-app'
+import React from 'react'
+import { Glyphicon, Tooltip, OverlayTrigger } from 'react-bootstrap'
+
+const glyphStyle = {
+  fontSize: 0.9 + 'em',
+  paddingLeft: 8,
+  cursor: 'pointer'
+}
+
+const EditEventGlyph = ({ event }) =>
+  <OverlayTrigger
+    placement='top'
+    overlay={
+      <Tooltip id='editThisEvent'>
+        edit
+      </Tooltip>
+    }
+  >
+    <Glyphicon
+      glyph='pencil'
+      style={glyphStyle}
+      onClick={() =>
+        app.Actions.getEvent(event._id)
+      }
+    />
+  </OverlayTrigger>
+
+EditEventGlyph.displayName = 'EditEventGlyph'
+
+EditEventGlyph.propTypes = {
+  event: React.PropTypes.object
+}
+
+export default EditEventGlyph
