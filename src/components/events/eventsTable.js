fee3f9339a40d079827b2b4e2685a4d7c2a0d607
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-05 13:34:21 +0200

    convert to module

--- src/components/events/eventsTable.js
+++ src/components/events/eventsTable.js
@@ -2,66 +2,71 @@ import React from 'react'
 import GeminiScrollbar from 'react-gemini-scrollbar'
 import DateRows from './dateRows.js'
 
-export default React.createClass({
-  displayName: 'Events',
+const Events = ({
+  introJumbotronHeight,
+  activeEventYears,
+  events,
+  email,
+  onRemoveEvent,
+}) => {
+  const eventsTableHeadTop = introJumbotronHeight ? introJumbotronHeight + 88 : 396
+  const eventsTableHeadStyle = {
+    top: eventsTableHeadTop,
+    position: 'absolute'
+  }
+  const fontSize = window.innerWidth < 500 ? 20 : 24
+  const headerStyle = { fontSize }
 
-  propTypes: {
-    events: React.PropTypes.array,
-    yearsOfEvents: React.PropTypes.array,
-    email: React.PropTypes.string,
-    introJumbotronHeight: React.PropTypes.number,
-    activeEventYears: React.PropTypes.array,
-    setActiveEventYears: React.PropTypes.func,
-    onRemoveEvent: React.PropTypes.func,
-  },
-
-  render() {
-    const { introJumbotronHeight, activeEventYears, events, email, onRemoveEvent } = this.props
-    const eventsTableHeadTop = introJumbotronHeight ? introJumbotronHeight + 88 : 396
-    const eventsTableHeadStyle = {
-      top: eventsTableHeadTop,
-      position: 'absolute'
-    }
-    const fontSize = window.innerWidth < 500 ? 20 : 24
-    const headerStyle = { fontSize }
-
-    return (
-      <div className="eventsTable">
-        <div
-          style={eventsTableHeadStyle}
-          className="eventsTable-header"
-        >
-          <div className="eventsTable-header-row">
-            <div
-              className="eventsTable-header-cell eventsTable-cell-day"
-              style={headerStyle}
-            >
-            </div>
-            <div
-              className="eventsTable-header-cell eventsTable-cell-migration"
-              style={headerStyle}
-            >
-              Maritime Events
-            </div>
-            <div
-              className="eventsTable-header-cell eventsTable-cell-politics"
-              style={headerStyle}
-            >
-              Political Events
-            </div>
+  return (
+    <div className="eventsTable">
+      <div
+        style={eventsTableHeadStyle}
+        className="eventsTable-header"
+      >
+        <div className="eventsTable-header-row">
+          <div
+            className="eventsTable-header-cell eventsTable-cell-day"
+            style={headerStyle}
+          >
+          </div>
+          <div
+            className="eventsTable-header-cell eventsTable-cell-migration"
+            style={headerStyle}
+          >
+            Maritime Events
+          </div>
+          <div
+            className="eventsTable-header-cell eventsTable-cell-politics"
+            style={headerStyle}
+          >
+            Political Events
           </div>
         </div>
-        <div className="eventsTable-body">
-          <GeminiScrollbar id="eventsTableBody" autoshow>
-            <DateRows
-              events={events}
-              email={email}
-              activeEventYears={activeEventYears}
-              onRemoveEvent={onRemoveEvent}
-            />
-          </GeminiScrollbar>
-        </div>
       </div>
-    )
-  }
-})
+      <div className="eventsTable-body">
+        <GeminiScrollbar id="eventsTableBody" autoshow>
+          <DateRows
+            events={events}
+            email={email}
+            activeEventYears={activeEventYears}
+            onRemoveEvent={onRemoveEvent}
+          />
+        </GeminiScrollbar>
+      </div>
+    </div>
+  )
+}
+
+Events.displayName = 'Events'
+
+Events.propTypes = {
+  events: React.PropTypes.array,
+  yearsOfEvents: React.PropTypes.array,
+  email: React.PropTypes.string,
+  introJumbotronHeight: React.PropTypes.number,
+  activeEventYears: React.PropTypes.array,
+  setActiveEventYears: React.PropTypes.func,
+  onRemoveEvent: React.PropTypes.func,
+}
+
+export default Events
